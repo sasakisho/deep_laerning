43a501fc85297669150592902e6e810746bd289e
sasakisho <sshou@systembase.co.jp>
2017-05-19 17:38:19 +0900

    ch5

--- bp_learn/layer_native.py
+++ bp_learn/layer_native.py
@@ -1,3 +1,6 @@
+from common.functions import *
+
+# 乗算レイヤ
 class MulLayer:
     def __init__(self):
         self.x=None
@@ -14,6 +17,7 @@ class MulLayer:
         dy=dout*self.x
         return dx,dy
 
+# 加算レイヤ
 class AddLayer:
     def __init__(self):
         pass
@@ -26,3 +30,71 @@ class AddLayer:
         dx=dout*1
         dy=dout*1
         return dx,dy
+
+# ReLUレイヤ
+class Relu:
+    def __init__(self):
+        self.mask=None
+
+    def forward(self,x):
+        self.mask=(x<=0)
+        out=x.copy()
+        out[self.mask]
+        return out
+
+    def backward(self,dout):
+        dout[self.mask]=0
+        dx=dout
+        return dx
+
+# シグモイドレイヤ
+class Sigmoid:
+    def __init__(self):
+        self.out=None
+
+    def forward(self,x):
+        out=1/(1+exp(-x))
+        self.out=out
+        return out
+
+    def backward(self,dout):
+        dx=dout*(1.0-self.out)*self.out
+        return dx
+
+# 内積レイヤ
+class Affine:
+    def __init__(self,W,b):
+        self.W=W
+        self.b=b
+        self.x=None
+        self.dW=None
+        self.db=None
+
+    def forward(self,x):
+        self.x=x
+        out=np.dot(x,self.W)+self.b
+        return out
+
+    def backward(self,dout):
+        dx=np.dot(dout,self.W.T)
+        self.dW=np.dot(self.x.T,dout)
+        self.db=np.sum(dout,axis=0)
+        return dx
+
+# ソフトマックスレイヤ(クロスエントロピー)
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss=None
+        self.y=None
+        self.t=None
+
+    def forward(self,x,t):
+        self.t=t
+        self.y=softmax(x)
+        self.loss=cross_entropy_error(self.y,self.t)
+        return self.loss
+
+    def backward(self,dout=1):
+        batch_size=self.t.shape[0]
+        dx=(self.y-self.t)/batch_size
+        return dx
